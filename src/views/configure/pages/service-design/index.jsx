55ff5a853b20909c5ce4e76cf402ed08c3d3cb75
wangkp <wangkp@citycloud.com.cn>
2023-06-26 17:35:52 +0800

    feat: 增加绘图功能

--- src/views/configure/pages/service-design/index.jsx
+++ src/views/configure/pages/service-design/index.jsx
@@ -1,4 +1,4 @@
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
 import { ElInputNumber, ElLink } from 'element-plus'
 import { Setting } from '@element-plus/icons-vue'
 import CipPageLayoutInfo from '@cip/components/page-layout/info'
@@ -13,6 +13,8 @@ import {
 
 import styles from './index.module.less'
 import './ausyda.css'
+import * as d3 from 'd3'
+import { Ausyda } from './ausyda.js'
 
 export default defineComponent({
   setup (props, ctx) {
@@ -23,6 +25,32 @@ export default defineComponent({
     const sourceCodeDialogVisible = ref(false)
     const compListDialogVisible = ref(false)
     const { showRightPanel, dialogBaseProps } = useDialog()
+
+    onMounted(() => {
+      d3.json('../../ausyda.json').then(data => {
+        const au = new Ausyda({
+          el: '#api-editor',
+          data
+        })
+        // 点击连接线上的+
+        au.on('addNode', (link) => {
+          // 添加节点
+          au.addNode({ id: Math.random().toString(16).slice(2), type: 'branch', title: 'http请求' }, link)
+        })
+        // 点击选中节点
+        au.on('updateNode', (d) => {
+          d.title = d.title + '2'
+          // 更新节点
+          au.updateNode(d)
+        })
+        // 点击节点删除按钮
+        au.on('deleteNode', (d, cb) => {
+          alert(d.title)
+          // 执行回调函数删除节点
+          cb()
+        })
+      })
+    })
 
     return () => <CipPageLayoutInfo>
       <div class={styles.page}>
@@ -42,7 +70,7 @@ export default defineComponent({
               <ElLink icon={Setting} underline={false} type='primary' onClick={() => { globalSettingVisible.value = true }}>全局设置</ElLink>
               <ElInputNumber size='small' v-model={zoom.value} min={50} max={400} step={10}></ElInputNumber>
             </div>
-            <div class={styles['view-container']}>画布区域</div>
+            <div class={styles['view-container']} id="api-editor"></div>
           </div>
           <div class={[styles['right-panel'], showRightPanel.value ? styles['right-panel-opened'] : '']}>
             <GlobalSettingDialog {...dialogBaseProps} v-model={globalSettingVisible.value}/>
