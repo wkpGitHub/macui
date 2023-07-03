7a539449c3b063d7ee275d184ffff78a7e997254
xmf <501235949@qq.com>
2023-07-03 16:31:23 +0800

    polish: 优化配置项

--- src/components/page-design/component-configure/page-layout/list.js
+++ src/components/page-design/component-configure/page-layout/list.js
@@ -1,3 +1,4 @@
+import { slotsCommonConfig } from '../slots-common-config'
 export default {
   key: {},
   title: {
@@ -7,30 +8,11 @@ export default {
     label: '是否可以返回',
     type: 'switch'
   },
-  usingSlots: {
-    label: '插槽选则',
-    type: 'select',
-    multiple: true,
-    realArray: true,
-    options: [
-      { value: 'filter', label: '搜索栏' },
-      { value: 'handle', label: '操作' },
-      { value: 'title', label: '表标题' },
-      { value: 'default', label: '默认' },
-      { value: 'pagination', label: '分页' }
-    ]
-  },
-  options: {
-    hideItem: 'true',
-    type: 'arrayObject',
-    dependOn: ['usingSlots', 'options'],
-    changeValue: ({ usingSlots, options }) => {
-      const value = options.concat(usingSlots.filter(name => {
-        return !options.find(option => option.key === name)
-      }).map(name => ({ key: name, children: [] })))
-      return {
-        value
-      }
-    }
-  }
+  ...slotsCommonConfig([
+    { value: 'filter', label: '搜索栏' },
+    { value: 'handle', label: '操作' },
+    { value: 'title', label: '表标题' },
+    { value: 'default', label: '默认' },
+    { value: 'pagination', label: '分页' }
+  ])
 }
