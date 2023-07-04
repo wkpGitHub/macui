5d2234fb849254ce4acae9698d90bcc751979602
d-render <39474671+d-render@users.noreply.github.com>
2023-07-04 12:48:12 +0800

    chore: 调整cci插件依赖

--- d-render.config.js
+++ d-render.config.js
@@ -2,6 +2,7 @@
 import CCIPlugin from '@cip/d-render-plugin-cci'
 import { insertConfig } from '@d-render/shared'
 import '@cip/d-render-plugin-cci/dist/index.css'
+import CCIPluginConfigure from '@cip/d-render-plugin-cci-configure'
 // 自定义的输入插件
 import customInputsPlugin from '@/components/custom-form-input/component-config'
 import customLayoutPlugin from '@/components/custom-form-layout/component-config'
@@ -17,5 +18,6 @@ export default {
     customInputsPlugin,
     customLayoutPlugin,
     pageDesignPlugin
-  ].map(v => insertConfig(v, customConfigurePlugin, 'configure')) // insertConfigureFile(v, customConfigurePlugin))
+  ].map(v => insertConfig(v, CCIPluginConfigure, 'configure'))
+    .map(v => insertConfig(v, customConfigurePlugin, 'configure')) // insertConfigureFile(v, customConfigurePlugin))
 }
